e891a65ffc08addcf4b3ded2bea33fedb90a3e8c
unknownproperty <igor.buksheff@yandex.ru>
2022-12-27 01:23:17 +0300

    feat(8421489): first commit for new tasks cards

--- apps/xi.front/components/Community/Tasks/Item.tsx
+++ apps/xi.front/components/Community/Tasks/Item.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { observer } from 'mobx-react';
-import { Box, Typography, Stack } from '@mui/material';
-import { useRouter, NextRouter } from 'next/router';
-import { Arrow } from 'pkg.icons.arrow';
-import { TaskT } from './types';
+import React from "react";
+import { observer } from "mobx-react";
+import {
+  Grid,
+  Box,
+  Typography,
+  Stack,
+  useMediaQuery,
+  Theme,
+} from "@mui/material";
+import { useRouter, NextRouter } from "next/router";
+import { Arrow } from "pkg.icons.arrow";
+import { TaskT } from "./types";
 
 type ItemsT = {
   item: TaskT;
@@ -12,143 +19,130 @@ type ItemsT = {
 
 const Item: React.FC<ItemsT> = observer(({ item, index }) => {
   const router: NextRouter = useRouter();
+  const mobile1336: boolean = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down(1336)
+  );
 
   const { mark, title, description } = item;
 
   const getColor = () => {
-    if (mark === 4 || mark === 5) return '#11743A';
-    if (mark === 3) return '#E75223';
-    return '#F42D2D';
+    if (mark === 4 || mark === 5) return "#11743A";
+    if (mark === 3) return "#E75223";
+    return "#F42D2D";
   };
 
   const getBgColor = () => {
-    if (mark === 4 || mark === 5) return '#B0F9CE';
-    if (mark === 3) return '#FEF2EB';
-    return '#FEEAEA';
+    if (mark === 4 || mark === 5) return "#B0F9CE";
+    if (mark === 3) return "#FEF2EB";
+    return "#FEEAEA";
   };
 
   return (
     <Stack
-      direction="column"
+      direction="row"
       justifyContent="flex-start"
-      alignItems="flex-start"
+      alignItems="center"
       onClick={() =>
-        router.push(`/community/${router.query.comid}/tasks/${router.query.chid}/task/${index}`)
+        router.push(
+          `/community/${router.query.comid}/tasks/${router.query.chid}/task/${index}`
+        )
       }
       sx={{
-        position: 'relative',
-        height: '224px',
-        backgroundColor: 'grayscale.0',
-        padding: 3,
-        textAlign: 'center',
-        width: '100%',
-        minWidth: '310px',
-        borderRadius: '8px',
-        cursor: 'pointer',
-
-        '&:after': {
-          content: `"${mark}"`,
-          position: 'absolute',
-          bgcolor: getBgColor(),
-          color: getColor(),
-          display: 'flex',
-          fontWeight: 600,
-          fontSize: '24px',
-          lineHeight: '32px',
-          justifyContent: 'center',
-          alignItems: 'center',
-          top: '0px',
-          right: '0px',
-          height: '32px',
-          width: '32px',
-          borderTopRightRadius: '8px',
-          borderBottomLeftRadius: '16px',
-        },
+        height: "198px",
+        backgroundColor: "grayscale.0",
+        textAlign: "center",
+        minWidth: mobile1336 ? "343px" : "504px",
+        borderRadius: "8px",
+        cursor: "pointer",
       }}
     >
-      <Typography
-        sx={{
-          fontWeight: 600,
-          fontSize: '20px',
-          lineHeight: '24px',
-          color: 'grayscale.100',
-        }}
-      >
-        {title}
-      </Typography>
-      <Box
-        sx={{
-          mt: 2,
-          height: '100%',
-        }}
-      >
-        <Typography
-          textAlign="left"
-          sx={{
-            fontWeight: 400,
-            fontSize: '16px',
-            lineHeight: '20px',
-            color: 'grayscale.80',
-          }}
-        >
-          {description}
-        </Typography>
-      </Box>
       <Stack
-        sx={{
-          width: '100%',
-          height: '24px',
-        }}
-        direction="row"
-        justifyContent="flex-start"
+        direction="column"
+        justifyContent="center"
         alignItems="center"
+        sx={{
+          bgcolor: "error.pale",
+          width: "80px",
+          height: "100%",
+          borderTopLeftRadius: "8px",
+          borderBottomLeftRadius: "8px",
+        }}
       >
         <Typography
-          textAlign="left"
           sx={{
-            fontWeight: 500,
-            fontSize: '12px',
-            lineHeight: '16px',
-            color: 'grayscale.100',
+            color: "error.dark",
+            fontWeight: 400,
+            fontSize: "12px",
+            lineHeight: "16px",
           }}
         >
-          3 мая 2022
+          Сегодня
         </Typography>
         <Typography
-          textAlign="left"
           sx={{
-            position: 'relative',
-            ml: '11px',
+            color: "error.dark",
             fontWeight: 500,
-            fontSize: '12px',
-            lineHeight: '16px',
-            color: 'grayscale.100',
-            '&:before': {
-              content: "''",
-              bgcolor: 'grayscale.100',
-              position: 'absolute',
-              left: '-7px',
-              top: '7px',
-              height: '3px',
-              width: '3px',
-              borderRadius: '100%',
-            },
+            fontSize: "16px",
+            lineHeight: "22px",
           }}
         >
-          Юшкевич О.А.
+          14:00
         </Typography>
       </Stack>
-      <Box
+      <Stack
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="flex-start"
         sx={{
-          width: '24px',
-          height: '24px',
-          position: 'absolute',
-          bottom: '24px',
-          right: '24px',
+          p: 2,
+          width: "100%",
+          height: "100%",
         }}
       >
-        <Arrow color="#445AFF" />
-      </Box>
+        <Box
+          sx={{
+            p: "4px 8px",
+            bgcolor: "error.pale",
+            color: "error.dark",
+            fontWeight: 500,
+            fontSize: "12px",
+            lineHeight: "16px",
+            borderRadius: "4px",
+          }}
+        >
+          Назначено
+        </Box>
+        <Typography
+          sx={{
+            mt: 1,
+            fontWeight: 600,
+            fontSize: "18px",
+            lineHeight: "24px",
+          }}
+        >
+          Расчет цикла ПХМ
+        </Typography>
+        {/* <Grid container wrap="nowrap" spacing={2}>
+          <Grid item xs zeroMinWidth> */}
+            <Typography
+              align="left"
+              // noWrap
+              sx={{
+                mt: 0.5,
+                fontWeight: 400,
+                fontSize: "16px",
+                lineHeight: "22px",
+                lineClamp: 3,
+              }}
+            >
+              Паровая холодильная машина работает по циклу с дросселированием.
+              Температура кипения в испарителе to,температура в конденсаторе tk.
+              В компрессор поступает пар с температурой t1.Рабочее тело перед
+              регулирующим вентилем переохлаждается до tпер.
+            </Typography>
+          {/* </Grid>
+        </Grid> */}
+      </Stack>
     </Stack>
   );
 });
