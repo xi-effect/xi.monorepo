1934f585b61f76525c77dd3b05b93aaaa4b038c5
igor.b <igor.b@ati.su>
2023-01-11 00:52:37 +0300

    feat: rename eslint

--- apps/xi.front/next.config.js
+++ apps/xi.front/next.config.js
@@ -31,7 +31,7 @@ const nextConfig = {
     outputFileTracingRoot: path.join(__dirname, "../../"),
   },
   transpilePackages: [
-    "pkg.eslint-config-custom",
+    "pkg.config.eslint",
     "pkg.theme.scheme",
     "pkg.components.loading",
     "pkg.data.avatar",
@@ -79,13 +79,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      "xieffect.pythonanywhere.com",
       "cdn.discordapp.com",
       "localhost:3000",
       "localhost:5000",
       "xieffect.ru:5000",
       "xieffect.ru",
-      "www.youtube.com",
     ],
   },
   output: "standalone",
