f8f5643a615be696e0105c0e585020d5d8d6e239
mariik <m.portnykh@fxdev.sk>
2023-01-17 18:46:03 +0100

    feat: add styles to file component

--- packages/pkg.components.file/File.tsx
+++ packages/pkg.components.file/File.tsx
@@ -1,4 +1,6 @@
-import { Stack, Typography } from '@mui/material';
+import { File as FileIcon } from 'pkg.icons.file';
+import { Close } from 'pkg.icons.close';
+import { Box, Stack, Typography } from '@mui/material';
 import Image from 'next/image';
 
 export type FileProps = {
@@ -19,11 +21,52 @@ const formatSize = (size: number): string => {
   return formattedSize;
 };
 
-export const File = ({ name, url, size }: FileProps) => (
-  <Stack>
-    <Typography>{name}</Typography>
-    <Image src={url} alt="test" width={100} height={100} />
-    <Typography>{formatSize(size)}</Typography>
+export const File = ({ name, size, icon }: FileProps) => (
+  <Stack
+    sx={{
+      maxWidth: '377px',
+      width: '100%',
+      minWidth: 0,
+      height: '72px',
+      padding: '14px 16px 14px 12px',
+      bgcolor: 'white',
+      mt: '20px',
+      border: '1px solid #E8E8E8',
+      borderRadius: '8px',
+    }}
+    direction="row"
+    justifyContent="space-between"
+    alignItems="center"
+  >
+    <Stack justifyContent="center" alignItems="center" sx={{ width: '64px', height: '48px' }}>
+      <Box sx={{ maxWidth: '48px', maxHeight: '48px' }}>
+        {(!icon && <FileIcon color="primary" height="100%" width="36px" viewBox="0 0 36 36" />) || (
+          <Image
+            src={icon || ''}
+            alt="test"
+            width={0}
+            height={0}
+            style={{
+              minWidth: 0,
+              width: '100%',
+              height: '100%',
+              maxHeight: '48px',
+              maxWidth: '48px',
+              display: 'inline-block',
+              borderRadius: 4,
+            }}
+          />
+        )}
+      </Box>
+    </Stack>
+
+    <Box sx={{ textAlign: 'left', width: '100%', m: '0 8px' }}>
+      <Typography sx={{ fontSize: '16px', fontWeight: 500, lineHeight: '22px' }}>{name}</Typography>
+      <Typography sx={{ fontSize: '14px', fontWeight: 400, lineHeight: '20px' }}>
+        {formatSize(size)}
+      </Typography>
+    </Box>
+    <Close />
   </Stack>
 );
 
