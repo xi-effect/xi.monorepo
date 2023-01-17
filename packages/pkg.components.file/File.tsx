1c7458ef435e5e3347c88c2c61016c9677d2926f
mariik <m.portnykh@fxdev.sk>
2023-01-17 18:50:31 +0100

    feat: add file downloading

--- packages/pkg.components.file/File.tsx
+++ packages/pkg.components.file/File.tsx
@@ -21,66 +21,68 @@ const formatSize = (size: number): string => {
   return formattedSize;
 };
 
-export const File = ({ name, size, icon, hideCloseIcon }: FileProps) => (
-  <Stack
-    sx={{
-      maxWidth: '377px',
-      width: '100%',
-      minWidth: 0,
-      height: '72px',
-      padding: '8px 14px 8px 12px',
-      bgcolor: 'white',
-      mt: '20px',
-      border: '1px solid #E8E8E8',
-      borderRadius: '8px',
-    }}
-    direction="row"
-    justifyContent="space-between"
-    alignItems="center"
-  >
-    <Stack justifyContent="center" alignItems="center" sx={{ width: '64px', height: '48px' }}>
-      {(!icon && (
-        <Box sx={{ maxWidth: '48px', maxHeight: '48px', height: '100%', width: '100%' }}>
-          <FileIcon
-            color="primary"
-            height="36px"
-            width="36px"
-            preserveAspectRatio="none"
-            sx={{
-              width: '100%',
-              height: '100%',
-              fontSize: '36px',
-            }}
-          />
-        </Box>
-      )) || (
-        <Box sx={{ maxWidth: '48px', maxHeight: '48px' }}>
-          <Image
-            src={icon || ''}
-            alt="test"
-            width={0}
-            height={0}
-            style={{
-              minWidth: 0,
-              width: '100%',
-              height: '100%',
-              maxHeight: '48px',
-              maxWidth: '48px',
-              display: 'inline-block',
-              borderRadius: 4,
-            }}
-          />
-        </Box>
-      )}
+export const File = ({ name, size, icon, hideCloseIcon, url }: FileProps) => (
+  <a href={url} download={name} style={{ maxWidth: '377px', width: '100%' }}>
+    <Stack
+      sx={{
+        width: '100%',
+        minWidth: 0,
+        height: '72px',
+        padding: '8px 14px 8px 12px',
+        bgcolor: 'grayscale.0',
+        mt: '20px',
+        border: '1px solid #E8E8E8',
+        borderRadius: '8px',
+      }}
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Stack justifyContent="center" alignItems="center" sx={{ width: '64px', height: '48px' }}>
+        {(!icon && (
+          <Box sx={{ maxWidth: '48px', maxHeight: '48px', height: '100%', width: '100%' }}>
+            <FileIcon
+              color="primary"
+              height="36px"
+              width="36px"
+              preserveAspectRatio="none"
+              sx={{
+                width: '100%',
+                height: '100%',
+                fontSize: '36px',
+              }}
+            />
+          </Box>
+        )) || (
+          <Box sx={{ maxWidth: '48px', maxHeight: '48px' }}>
+            <Image
+              src={icon || ''}
+              alt="test"
+              width={0}
+              height={0}
+              style={{
+                minWidth: 0,
+                width: '100%',
+                height: '100%',
+                maxHeight: '48px',
+                maxWidth: '48px',
+                display: 'inline-block',
+                borderRadius: 4,
+              }}
+            />
+          </Box>
+        )}
+      </Stack>
+
+      <Box sx={{ textAlign: 'left', width: '100%', m: '0 8px' }}>
+        <Typography sx={{ fontSize: '16px', fontWeight: 500, lineHeight: '22px' }}>
+          {name}
+        </Typography>
+        <Typography sx={{ fontSize: '14px', fontWeight: 400, lineHeight: '20px' }}>
+          {formatSize(size)}
+        </Typography>
+      </Box>
+      {!hideCloseIcon && <Close />}
     </Stack>
-
-    <Box sx={{ textAlign: 'left', width: '100%', m: '0 8px' }}>
-      <Typography sx={{ fontSize: '16px', fontWeight: 500, lineHeight: '22px' }}>{name}</Typography>
-      <Typography sx={{ fontSize: '14px', fontWeight: 400, lineHeight: '20px' }}>
-        {formatSize(size)}
-      </Typography>
-    </Box>
-    {!hideCloseIcon && <Close />}
-  </Stack>
+  </a>
 );
-
