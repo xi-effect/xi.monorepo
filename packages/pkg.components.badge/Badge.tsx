c65e6bb27494b6cb237b84c4faef8ed8bd2b6f73
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-01-23 14:31:49 +0300

    fix: refactoring code style

--- packages/pkg.components.badge/Badge.tsx
+++ packages/pkg.components.badge/Badge.tsx
@@ -1,17 +1,11 @@
-import {
-  Typography,
-  TypographyProps,
-  Box,
-  BoxProps,
-  SvgIconProps,
-} from '@mui/material';
+import { Typography, TypographyProps, Box, BoxProps, SvgIconProps } from '@mui/material';
 
 import { FC, FunctionComponent } from 'react';
 
 export type BadgeProps = {
   Icon?: FunctionComponent<SvgIconProps>;
   text: string;
-  bgColor:string;
+  bgColor: string;
   size?: 'small' | 'heigh';
   boxProps?: BoxProps;
   typographyProps?: TypographyProps;
@@ -30,36 +24,36 @@ export const Badge: FC<BadgeProps> = ({
   <Box
     component="span"
     sx={{
-        display: 'flex',
-        alignItems: 'center',
-        width: 'fit-content',
-        gap: size === 'heigh' ? '8px' : '7.5px',
-        height: size === 'heigh' ? '28px' : '20px',
-        borderRadius: size === 'heigh' ? '6px' : '4px',
-        padding: size === 'heigh' ? '4px 8px 4px 10px ' : '2px 6px',
-        backgroundColor: bgColor,
-      }}
+      display: 'flex',
+      alignItems: 'center',
+      width: 'fit-content',
+      gap: size === 'heigh' ? '8px' : '7.5px',
+      height: size === 'heigh' ? '28px' : '20px',
+      borderRadius: size === 'heigh' ? '6px' : '4px',
+      padding: size === 'heigh' ? '4px 8px 4px 10px ' : '2px 6px',
+      backgroundColor: bgColor,
+    }}
     {...boxProps}
   >
     {Icon && (
-    <Icon
-      sx={{
-            width: size === 'heigh' ? '16px' : '12px',
-            height: size === 'heigh' ? '16px' : '12px',
-          }}
-      {...iconProps}
-    />
-      )}
+      <Icon
+        sx={{
+          width: size === 'heigh' ? '16px' : '12px',
+          height: size === 'heigh' ? '16px' : '12px',
+        }}
+        {...iconProps}
+      />
+    )}
     <Typography
       component="span"
       sx={{
-          fontWeight: '500',
-          fontSize: size === 'heigh' ? '14px' : '12px',
-          lineHeight: size === 'heigh' ? '20px' : '16px',
-        }}
+        fontWeight: '500',
+        fontSize: size === 'heigh' ? '14px' : '12px',
+        lineHeight: size === 'heigh' ? '20px' : '16px',
+      }}
       {...typographyProps}
     >
       {text}
     </Typography>
   </Box>
-  );
+);
