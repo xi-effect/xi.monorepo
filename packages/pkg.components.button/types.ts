5a6fc0dc56779eff620fa9d3471c74f6fbc5fb41
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-01-25 16:05:29 +0300

    feat: add types file

---
+++ packages/pkg.components.button/types.ts
@@ -0,0 +1,5 @@
+export type Status = 'idle' | 'pending' | 'completed';
+export type Size = 'small' | 'medium' | 'large';
+export type LoadingPosition = 'start' | 'center' | 'end';
+export type Variant = 'contained' | 'outlined' | 'text';
+export type Color = 'primary' | 'confirm' | 'reject';
