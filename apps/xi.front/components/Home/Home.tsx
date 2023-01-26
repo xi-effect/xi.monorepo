4efafd4e6a1a7a8ae60b84367956a30c624faf21
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-01-27 02:37:53 +0300

    feat: add test component

--- apps/xi.front/components/Home/Home.tsx
+++ apps/xi.front/components/Home/Home.tsx
@@ -1,23 +1,38 @@
 import { Button } from 'pkg.components.button';
 import { Camera } from 'pkg.icons.camera';
+import { useState } from 'react';
 import Communities from './Communities';
 import Header from './Header';
 
-const Home = () => (
-  <>
-    <Header />
-    <Communities />
-    <Button
-      startIcon={Camera}
-      size="large"
-      status="idle"
-      textColor="white"
-      text="Кнопка"
-      loadingPosition="center"
-      isSnackbar
-      handleButtonClick={() => {}}
-    />
-  </>
-);
+const Home = () => {
+  const [status, setStatus] = useState<'idle' | 'pending' | 'completed'>('idle');
+  return (
+    <>
+      <Header />
+      <Communities />
+      <Button
+        startIcon={Camera}
+        text="Кнопка"
+        loadingPosition="icon"
+        size="large"
+        status={status}
+        iconColor="white"
+        snackbarText="123"
+        isSnackbar
+        snackbarLoadingPosition="icon"
+        isSnackbarIconEnd
+        handleButtonClick={() => {
+          setStatus('pending');
+          setTimeout(() => {
+            setStatus('completed');
+            setTimeout(() => {
+              setStatus('idle');
+            }, 1500);
+          }, 1500);
+        }}
+      />
+    </>
+  );
+};
 
 export default Home;
