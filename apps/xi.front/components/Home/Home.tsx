dcf29554ec06a42ff646fe932d457de974b11646
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-01-27 02:39:14 +0300

    fix: rm test component

--- apps/xi.front/components/Home/Home.tsx
+++ apps/xi.front/components/Home/Home.tsx
@@ -1,38 +1,11 @@
-import { Button } from 'pkg.components.button';
-import { Camera } from 'pkg.icons.camera';
-import { useState } from 'react';
 import Communities from './Communities';
 import Header from './Header';
 
-const Home = () => {
-  const [status, setStatus] = useState<'idle' | 'pending' | 'completed'>('idle');
-  return (
-    <>
-      <Header />
-      <Communities />
-      <Button
-        startIcon={Camera}
-        text="Кнопка"
-        loadingPosition="icon"
-        size="large"
-        status={status}
-        iconColor="white"
-        snackbarText="123"
-        isSnackbar
-        snackbarLoadingPosition="icon"
-        isSnackbarIconEnd
-        handleButtonClick={() => {
-          setStatus('pending');
-          setTimeout(() => {
-            setStatus('completed');
-            setTimeout(() => {
-              setStatus('idle');
-            }, 1500);
-          }, 1500);
-        }}
-      />
-    </>
-  );
-};
+const Home = () => (
+  <>
+    <Header />
+    <Communities />
+  </>
+);
 
 export default Home;
