b4e285ef7b58361be83fbe152a92edffe84d03b9
mariik <m.portnykh@fxdev.sk>
2023-01-28 21:52:25 +0100

    feat: add test checbox components

--- apps/xi.front/pages/testcomponents/index.tsx
+++ apps/xi.front/pages/testcomponents/index.tsx
@@ -3,23 +3,62 @@ import { Stack } from '@mui/material';
 import { Checkbox, CheckboxProps } from 'pkg.components.checkbox';
 
 const TestComponents = () => {
-  const [isChecked, setIsChecked] = useState(false);
-  const changeCheckedState = () => {
-    setIsChecked((state) => !state);
+  const [isChecked1, setIsChecked1] = useState(false);
+  const [isChecked2, setIsChecked2] = useState(false);
+  const [isChecked3, setIsChecked3] = useState(false);
+  const [isChecked4, setIsChecked4] = useState(false);
+  const changeCheckedState1 = () => {
+    setIsChecked1((state) => !state);
+  };
+  const changeCheckedState2 = () => {
+    setIsChecked2((state) => !state);
+  };
+  const changeCheckedState3 = () => {
+    setIsChecked3((state) => !state);
+  };
+  const changeCheckedState4 = () => {
+    setIsChecked4((state) => !state);
   };
 
-  const TestChecboxData: CheckboxProps = {
+  const TestChecbox1: CheckboxProps = {
+    size: 'l',
+    icon: 'minus',
+    // type: 'warning',
+    label: 'Label',
+    isChecked: isChecked1,
+    changeCheckedState: changeCheckedState1,
+  };
+  const TestChecbox2: CheckboxProps = {
     size: 'l',
     // icon: 'minus',
     type: 'warning',
     label: 'Label',
-    isChecked,
-    changeCheckedState,
+    isChecked: isChecked2,
+    changeCheckedState: changeCheckedState2,
+  };
+  const TestChecbox3: CheckboxProps = {
+    size: 'l',
+    // icon: 'minus',
+    type: 'error',
+    label: 'Label',
+    isChecked: isChecked3,
+    changeCheckedState: changeCheckedState3,
+  };
+  const TestChecbox4: CheckboxProps = {
+    size: 'l',
+    // icon: 'minus',
+    type: 'disabled',
+    label: 'Label',
+    isChecked: isChecked4,
+    changeCheckedState: changeCheckedState4,
   };
 
   return (
-    <Stack sx={{ width: '100vw', height: '100vh', padding: '50px' }} direction="row">
-      <Checkbox {...TestChecboxData} />
+    <Stack sx={{ width: '100vw', height: '100vh', padding: '50px' }} direction="row" spacing={1}>
+      <Checkbox {...TestChecbox1} />
+      <Checkbox {...TestChecbox2} />
+      <Checkbox {...TestChecbox3} />
+      <Checkbox {...TestChecbox4} />
     </Stack>
   );
 };
