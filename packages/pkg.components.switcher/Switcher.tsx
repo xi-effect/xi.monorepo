eea28cde67a6ab1c96c22e244bd2dc644fdfdd8a
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-01-31 04:50:29 +0300

    feat: add style and component

--- packages/pkg.components.switcher/Switcher.tsx
+++ packages/pkg.components.switcher/Switcher.tsx
@@ -1,5 +1,87 @@
-import { FC } from 'react';
+import 'pkg.config.muidts';
+import { ToggleButtonGroup, ToggleButton, Typography } from '@mui/material';
+import { FC, MouseEvent, useState } from 'react';
 
-export const Switcher: FC<SwitcherProps> = () => <div>Switcher</div>;
+import {
+  buttonSizes,
+  typographyVariants,
+  buttonStyle,
+  buttonBorderRadius,
+  groupSizes,
+} from './styles';
 
-type SwitcherProps = {};
+export const Switcher: FC<SwitcherProps> = ({
+  values,
+  size = 'large',
+  color = 'primary',
+  groupBackgroundColor,
+  defaultValue,
+  disabledValue,
+  isError,
+  onChangeHandler,
+}) => {
+  const foundIndex = values.findIndex((value) => value === defaultValue);
+  const currentDefaultValue = foundIndex > -1 ? values[foundIndex] : '';
+
+  const [alignment, setAlignment] = useState(currentDefaultValue);
+
+  const handleChange = (event: MouseEvent<HTMLElement>, newAlignment: string) => {
+    setAlignment(newAlignment);
+    onChangeHandler(newAlignment, event);
+  };
+
+  return (
+    <ToggleButtonGroup
+      sx={{
+        border: '2px solid',
+        borderColor: isError ? 'error.dark' : 'transparent',
+        backgroundColor: groupBackgroundColor || 'transparent',
+        ...groupSizes[size],
+      }}
+      color="primary"
+      value={alignment}
+      onChange={handleChange}
+      exclusive
+    >
+      {values.map((value, i) => (
+        <ToggleButton
+          disableFocusRipple
+          disableRipple
+          disabled={disabledValue === value}
+          sx={{
+            border: 'none',
+            ...buttonSizes[size],
+            ...buttonStyle[color],
+            ...buttonStyle.default,
+            '&.MuiToggleButtonGroup-grouped:not(:first-of-type)': {
+              borderRadius: buttonBorderRadius[size],
+              marginLeft: 0,
+              borderLeft: 'none',
+            },
+            '&.MuiToggleButtonGroup-grouped:not(:last-of-type)': {
+              borderRadius: buttonBorderRadius[size],
+            },
+            borderRadius: buttonBorderRadius[size],
+          }}
+          key={`${i}`}
+          value={value}
+        >
+          <Typography variant={typographyVariants[size]} sx={{ lineHeight: '0' }}>
+            {value}
+          </Typography>
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  );
+};
+
+type SwitcherProps = {
+  values: string[] | number[];
+  size?: 'large' | 'medium' | 'small';
+  color?: 'primary' | 'white';
+  groupBackgroundColor?: string;
+  defaultValue?: string | number;
+  disabledValue?: string | number;
+  isError?: boolean;
+  onChangeHandler: (newAlignment?: string, event?: MouseEvent<HTMLElement>) => void;
+};
---
+++ packages/pkg.components.switcher/styles.ts
@@ -0,0 +1,63 @@
+export const buttonSizes = {
+  large: {
+    height: '34px',
+    p: '0 16px',
+  },
+  medium: {
+    height: '26px',
+    p: '0 12px',
+  },
+  small: {
+    height: '20px',
+    p: '0 12px',
+  },
+};
+
+export const buttonBorderRadius = {
+  large: '6px',
+  medium: '6px',
+  small: '4px',
+};
+
+export const buttonStyle = {
+  default: {
+    color: '#404040',
+    textTransform: 'none',
+    bgcolor: 'transparent',
+    '&:hover': {
+      bgcolor: 'transparent',
+    },
+  },
+  primary: {
+    '&.Mui-selected': {
+      color: 'grayscale.0',
+      bgcolor: 'primary.dark',
+      '&:hover': {
+        bgcolor: 'primary.dark',
+      },
+    },
+    '&:disabled': { border: 'none', color: 'grayscale.0', bgcolor: 'primary.dark', opacity: '.5' },
+  },
+  white: {
+    '&.Mui-selected': {
+      color: '#282828',
+      bgcolor: 'grayscale.0',
+      '&:hover': {
+        bgcolor: 'grayscale.0',
+      },
+    },
+    '&:disabled': { border: 'none', color: '#404040', bgcolor: '#E8E8E8', opacity: '.5' },
+  },
+};
+
+export const typographyVariants = {
+  large: 'm' as 'm',
+  medium: 's' as 's',
+  small: 'xs' as 'xs',
+};
+
+export const groupSizes = {
+  large: { borderRadius: '8px', p: '3px' },
+  medium: { borderRadius: '8px', p: '3px' },
+  small: { borderRadius: '6px', p: '2px' },
+};
