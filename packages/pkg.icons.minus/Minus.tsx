d0a04a2fdc886e6453f7d97a0d4df6c65b12c796
mariik <m.portnykh@fxdev.sk>
2023-02-01 14:18:35 +0100

    fix: checkbox minus icon

--- packages/pkg.icons.minus/Minus.tsx
+++ packages/pkg.icons.minus/Minus.tsx
@@ -6,6 +6,9 @@ export type MinusProps = {
 
 export const Minus = ({ ...props }: MinusProps) => (
   <SvgIcon {...props}>
-    <rect x="4" y="11" width="16" height="2" rx="0.5" fill="inherit" />
+    <path
+      d="M4.252 11.078C4.03 11.213 4 11.325 4 12s.03.787.252.922c.126.076.261.078 7.748.078s7.622-.002 7.748-.078c.222-.135.252-.247.252-.922s-.03-.787-.252-.922C19.622 11.002 19.487 11 12 11s-7.622.002-7.748.078"
+      fillRule="evenodd"
+    />
   </SvgIcon>
 );
