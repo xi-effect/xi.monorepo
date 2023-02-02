dcc7ea1e4e27a4718c2ddf9dbc7bc1df65763265
mariik <m.portnykh@fxdev.sk>
2023-02-02 14:11:29 +0100

    feat: add basic content

--- apps/xi.front/components/Home/Updates/Conetnt.tsx
+++ apps/xi.front/components/Home/Updates/Conetnt.tsx
@@ -1,6 +1,12 @@
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { observer } from 'mobx-react';
 
-const Content = observer(() => <Stack>Content</Stack>);
+const Content = observer(() => (
+  <Stack alignItems="center" justifyContent="center" sx={{ height: '80%', p: '186px' }}>
+    <Typography variant="l" sx={{ color: 'secondary.dark' }}>
+      Content
+    </Typography>
+  </Stack>
+));
 
 export default Content;
--- apps/xi.front/components/Home/Updates/Item.tsx
+++ apps/xi.front/components/Home/Updates/Item.tsx
@@ -1,7 +1,24 @@
 import { observer } from 'mobx-react';
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { UpdateT } from './types';
 
-const Item = observer((data: UpdateT) => <Stack>{data.title}</Stack>);
+const Item = observer((data: UpdateT) => {
+  const Content = data.content;
+
+  return (
+    <Stack sx={{ bgcolor: 'grayscale.0', borderRadius: '16px', p: '24px' }} spacing={2}>
+      <Stack spacing={0.5}>
+        <Typography variant="h4">{data.title}</Typography>
+        <Typography sx={{ color: 'grayscale.80', fontSize: '18px', lineHeight: '24px' }}>
+          {data.description}
+        </Typography>
+      </Stack>
+      <Stack>user etc...</Stack>
+      <Stack sx={{ bgcolor: 'secondary.pale', borderRadius: '16px' }}>
+        <Content />
+      </Stack>
+    </Stack>
+  );
+});
 
 export default Item;
