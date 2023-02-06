9b965f63b1fa5e84a31fac4520c9271e581879c1
mariik <m.portnykh@fxdev.sk>
2023-02-06 22:03:52 +0100

    feat: create pkg.inputs.select

---
+++ apps/xi.front/pages/testcomponents/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+import { Select } from 'pkg.inputs.select';
+
+const TestComponents = () => (
+  <Stack sx={{ width: '100vw', height: '100vh', padding: '50px' }}>
+    <Select />
+  </Stack>
+);
+
+export default TestComponents;
---
+++ packages/pkg.inputs.select/Select.tsx
@@ -0,0 +1,9 @@
+import { MenuItem, Select as MuiSelect } from '@mui/material';
+
+export const Select = () => (
+  <MuiSelect>
+    <MenuItem>1</MenuItem>
+    <MenuItem>2</MenuItem>
+    <MenuItem>3</MenuItem>
+  </MuiSelect>
+);
