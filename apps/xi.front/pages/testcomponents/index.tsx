3331d3d36f6a57f1c4f1dc48cb6eed2157bb2305
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-02-09 12:38:09 +0300

    fix: refactoring test

--- apps/xi.front/pages/testcomponents/index.tsx
+++ apps/xi.front/pages/testcomponents/index.tsx
@@ -8,34 +8,36 @@ const TestComponents = () => {
   const arr2 = ['I', 'II', 'III', 'IV'];
 
   const [value1, setValue1] = useState(arr1[0]);
-  const [value2, setValue2] = useState('');
+  const [value2, setValue2] = useState(arr1[0]);
+  const [value3, setValue3] = useState(arr1[0]);
+  const [value4, setValue4] = useState('');
 
   const onChangeValue1 = (value: string) => {
     setValue1(value);
   };
   const onChangeValue2 = (value: string) => {
-    setIsError(false);
     setValue2(value);
+  };
+  const onChangeValue3 = (value: string) => {
+    setValue3(value);
+  };
+  const onChangeValue4 = (value: string) => {
+    setIsError(false);
+    setValue4(value);
   };
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', padding: '10px' }}>
+      <Switcher size="small" currentValue={value1} values={arr1} onChange={onChangeValue1} />
+      <Switcher size="medium" currentValue={value2} values={arr1} onChange={onChangeValue2} />
+      <Switcher size="large" currentValue={value3} values={arr1} onChange={onChangeValue3} />
       <Switcher
         size="medium"
-        currentValue={value1}
-        values={arr1}
-        onChange={onChangeValue1}
-        disabledValue="Четвёртый"
-      />
-
-      <Switcher
-        size="medium"
-        currentValue={value2}
+        currentValue={value4}
         groupBackgroundColor="grayscale.10"
         values={arr2}
         color="white"
-        onChange={onChangeValue2}
+        onChange={onChangeValue4}
         isError={isError}
-        disabledValue="IV"
       />
     </div>
   );
