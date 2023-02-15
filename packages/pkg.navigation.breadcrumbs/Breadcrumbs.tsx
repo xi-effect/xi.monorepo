2418adb1b9c068958de621c67fc035d00a9cd88d
mariik <m.portnykh@fxdev.sk>
2023-02-15 18:12:28 +0100

    feat: add custom separator

--- packages/pkg.navigation.breadcrumbs/Breadcrumbs.tsx
+++ packages/pkg.navigation.breadcrumbs/Breadcrumbs.tsx
@@ -1,14 +1,34 @@
 import { Breadcrumbs as MuiBreadcrumbs, Link } from '@mui/material';
+import { Arrow } from 'pkg.icons.arrow';
 import { breadcrumbLink } from './types';
 
 export type BreadcrumbsProps = {
   breadcrumbs: breadcrumbLink[];
+  Separator?: any;
 };
 
-export const Breadcrumbs = ({ breadcrumbs }: BreadcrumbsProps) => (
-  <MuiBreadcrumbs>
-    {breadcrumbs.map((item) => (
-      <Link href={item.link}>{item.name}</Link>
-    ))}
+const DefaultSeparator = <Arrow sx={{ fontSize: '12px' }} />;
+
+export const Breadcrumbs = ({ breadcrumbs, Separator = DefaultSeparator }: BreadcrumbsProps) => (
+  <MuiBreadcrumbs separator={Separator}>
+    {breadcrumbs.map((item, index, breadcrumbsArray) => {
+      const isLastItem = index === breadcrumbsArray.length - 1;
+      const color = !isLastItem ? 'grayscale.40' : 'grayscale.100';
+      return (
+        <Link
+          href={item.link}
+          sx={{
+            color,
+            pointerEvents: isLastItem ? 'none' : '',
+            textDecoration: 'none',
+            '&:hover': {
+              textDecoration: isLastItem ? 'none' : 'underline',
+            },
+          }}
+        >
+          {item.name}
+        </Link>
+      );
+    })}
   </MuiBreadcrumbs>
 );
