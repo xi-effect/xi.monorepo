4e12e53719ae9f386078cd78f790c9b9163f60d9
mariik <m.portnykh@fxdev.sk>
2023-02-15 18:16:22 +0100

    feat: add custom color to breadcrumbs

--- packages/pkg.navigation.breadcrumbs/Breadcrumbs.tsx
+++ packages/pkg.navigation.breadcrumbs/Breadcrumbs.tsx
@@ -5,20 +5,30 @@ import { breadcrumbLink } from './types';
 export type BreadcrumbsProps = {
   breadcrumbs: breadcrumbLink[];
   Separator?: any;
+  /* breadcrumbs link color */
+  color?: string;
+  /* breadcrumbs last link color */
+  lastItemColor?: string;
 };
 
 const DefaultSeparator = <Arrow sx={{ fontSize: '12px' }} />;
 
-export const Breadcrumbs = ({ breadcrumbs, Separator = DefaultSeparator }: BreadcrumbsProps) => (
+export const Breadcrumbs = ({
+  breadcrumbs,
+  Separator = DefaultSeparator,
+  color = 'grayscale.100',
+  lastItemColor = 'grayscale.40',
+}: BreadcrumbsProps) => (
   <MuiBreadcrumbs separator={Separator}>
     {breadcrumbs.map((item, index, breadcrumbsArray) => {
       const isLastItem = index === breadcrumbsArray.length - 1;
-      const color = !isLastItem ? 'grayscale.40' : 'grayscale.100';
+      const linkColor = !isLastItem ? lastItemColor : color;
+
       return (
         <Link
           href={item.link}
           sx={{
-            color,
+            color: linkColor,
             pointerEvents: isLastItem ? 'none' : '',
             textDecoration: 'none',
             '&:hover': {
