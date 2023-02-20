c47f4cd78924eea25656bcc9895ded0473dbcadd
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-02-20 04:02:03 +0300

    feat: add test component

--- apps/xi.workshop/next.config.js
+++ apps/xi.workshop/next.config.js
@@ -50,6 +50,7 @@ const nextConfig = {
     'pkg.icons.copy',
     'pkg.icons.updates',
     'pkg.icons.photo',
+    'pkg.inputs.radio',
   ],
   compiler: {
     removeConsole: process.env.NODE_ENV !== 'development',
