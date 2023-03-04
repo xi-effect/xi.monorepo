c1441028ce58f093934dded48a009643a85b56e8
mariik <m.portnykh@fxdev.sk>
2023-03-04 19:49:35 +0100

    feat: add open dropdown menu

---
+++ packages/pkg.navigation.dropdown/styles.ts
@@ -0,0 +1,16 @@
+export const MenuProps = {
+  '&.Dropdown-root': {
+    width: '100px',
+    height: '100%',
+
+    '& .MuiBackdrop-root': {
+      bgcolor: 'unset',
+      width: 0,
+      height: 0,
+    },
+
+    '.MuiMenu-paper': {
+      minWidth: '242px',
+    },
+  },
+};
