d27026d720cb9e91146b3e7060c6366e5f3ef9a2
mariik <m.portnykh@fxdev.sk>
2023-03-04 19:49:35 +0100

    feat: add open dropdown menu

--- packages/pkg.navigation.dropdown/Dropdown.tsx
+++ packages/pkg.navigation.dropdown/Dropdown.tsx
@@ -1,10 +1,41 @@
-import { Button, Menu, MenuItem } from '@mui/material';
+import { Button, Menu, MenuItem, ClickAwayListener } from '@mui/material';
+import React, { useState } from 'react';
+import { MenuProps } from './styles';
 
-export const Dropdown = () => (
-  <>
-    <Button aria-controls="dropdown">Open menu</Button>
-    <Menu id="dropdown" open={false}>
-      <MenuItem>Item 1</MenuItem>
-    </Menu>
-  </>
-);
+export const Dropdown = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isOpened = Boolean(anchorEl);
+
+  const onOpenMenu = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (isOpened) setAnchorEl(null);
+    if (!isOpened) setAnchorEl(e.currentTarget);
+  };
+  const onCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <ClickAwayListener onClickAway={onCloseMenu}>
+        <Button aria-controls="dropdown" onClick={onOpenMenu}>
+          Open menu
+        </Button>
+      </ClickAwayListener>
+      <Menu
+        id="dropdown"
+        open={isOpened}
+        onClick={onCloseMenu}
+        onChange={(e) => console.log(e.target)}
+        anchorEl={anchorEl}
+        PopoverClasses={{
+          root: 'Dropdown-root',
+        }}
+        sx={{
+          ...MenuProps,
+        }}
+      >
+        <MenuItem>Item 1</MenuItem>
+      </Menu>
+    </>
+  );
+};
