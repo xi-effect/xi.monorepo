bfe2c6223f6e6815ef22c52d1176cc921426bda1
mariik <m.portnykh@fxdev.sk>
2023-03-06 17:17:01 +0100

    feat: add styles to dropdown

--- packages/pkg.navigation.dropdown/styles.ts
+++ packages/pkg.navigation.dropdown/styles.ts
@@ -11,6 +11,12 @@ export const MenuProps = {
 
     '.MuiMenu-paper': {
       minWidth: '242px',
+      border: '1px solid',
+      borderColor: 'grayscale.10',
+      borderRadius: '8px',
+      boxShadow: '0px 16px 8px rgba(16, 16, 16, 0.04), 0px 12px 16px rgba(16, 16, 16, 0.04)',
+      p: '4px',
+      mt: '4px',
     },
   },
 };
