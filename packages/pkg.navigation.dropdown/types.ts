1ab305f4b8fd28475c5aa0c8e36f19385bc1fb08
mariik <m.portnykh@fxdev.sk>
2023-03-13 17:30:19 +0100

    fix: add dropdown element prop

--- packages/pkg.navigation.dropdown/types.ts
+++ packages/pkg.navigation.dropdown/types.ts
@@ -1,9 +1,9 @@
-import { ReactElement } from 'react';
+import { FunctionComponent, ReactElement } from 'react';
 
 export type DropdownSizesT = 's' | 'm' | 'l';
 
 export type DropdownPropsT = {
-  name: string;
+  Element: FunctionComponent<any>;
   size?: DropdownSizesT;
   children?: ReactElement;
 };
