82eca968257e2707ace5a7d56827c20972015197
alexAskerko <askerko50@gmail.com>
2023-03-19 13:44:48 +0300

    feat(9328294):root access

--- apps/xi.front/kit/CommunityProfile/ContentItems/Roles/editRole/EditRole.tsx
+++ apps/xi.front/kit/CommunityProfile/ContentItems/Roles/editRole/EditRole.tsx
@@ -5,10 +5,13 @@ import {
   Button,
   FormControlLabel,
   IconButton,
+  InputAdornment,
   Paper,
   Radio,
   RadioGroup,
   Stack,
+  Switch,
+  TextField,
   Theme,
   Typography,
   useMediaQuery,
@@ -16,7 +19,9 @@ import {
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import SearchIcon from '@mui/icons-material/Search';
 import CheckIcon from '@mui/icons-material/Check';
+
 import { styled } from '@mui/material/styles';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Input } from 'pkg.inputs.input';
@@ -26,6 +31,50 @@ import * as yup from 'yup';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { yupResolver } from '@hookform/resolvers/yup';
 
+// root access
+type rootAccessType = {
+  title: string;
+  text: string;
+  name:
+    | 'viewAllChannel'
+    | 'manageRole'
+    | 'viewActivity'
+    | 'manageInvite'
+    | 'managePerson'
+    | 'manageChannels';
+};
+const rootAccess: rootAccessType[] = [
+  {
+    title: 'Просматривать все каналы',
+    text: 'Позволяет участникам просматривать все категории',
+    name: 'viewAllChannel',
+  },
+  {
+    title: 'Управлять ролями',
+    text: 'Позволяет участникам редактировать права ролей, которые ниже их самой роли',
+    name: 'manageRole',
+  },
+  {
+    title: 'Просматривать активность',
+    text: 'Позволяет участникам просматривать историю изменений этого сообщества',
+    name: 'viewActivity',
+  },
+  {
+    title: 'Управлять приглашениями',
+    text: 'Позволяет участникам удалять приглашения',
+    name: 'manageInvite',
+  },
+  {
+    title: 'Управлять участниками',
+    text: 'Позволяет участникам выгонять и банить других участников, которые ниже их самой роли',
+    name: 'managePerson',
+  },
+  {
+    title: 'Управление каналами',
+    text: 'Позволяет участникам создавать и редактировать объявления и задания',
+    name: 'manageChannels',
+  },
+];
 // ColorData
 const roleColorData = [
   { color: '#B4BDFF' },
@@ -330,7 +379,6 @@ const EditRole = observer(() => {
                       <RadioGroup defaultValue="#B4BDFF" {...field}>
                         <Stack
                           direction="row"
-                          // justifyContent={mobile700 ?"space-between":"flex-start"}
                           flexWrap="wrap"
                           sx={{ width: mobile700 ? '100%' : 340, marginLeft: '12px' }}
                           columnGap={0.5}
@@ -363,10 +411,70 @@ const EditRole = observer(() => {
               </Stack>
             </TabPanel>
             <TabPanel value={value} index={1}>
-              Item Two
+              <Stack mt={3}>
+                {rootAccess.map((r, index) => (
+                  <Stack
+                    key={index}
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                    sx={{
+                      width: '100%',
+                      mb: 3,
+                    }}
+                  >
+                    <Stack>
+                      <Typography
+                        sx={{
+                          // mb: 1,
+                          mr: 'auto',
+                          fontSize: '16px',
+                          fontWeight: 500,
+                          lineHeight: '22px',
+                        }}
+                      >
+                        {r.title}
+                      </Typography>
+                      <Typography
+                        sx={{
+                          // mb: 1,
+                          mr: 'auto',
+                          fontSize: '14px',
+                          fontWeight: 400,
+                          lineHeight: '20px',
+                        }}
+                      >
+                        {r.text}
+                      </Typography>
+                    </Stack>
+                    <Controller
+                      name={r.name}
+                      control={control}
+                      render={({ field: { onChange, value, ...restProps } }) => (
+                        <Switch size="medium" {...restProps} onChange={onChange} checked={value} />
+                      )}
+                    />
+                  </Stack>
+                ))}
+              </Stack>
             </TabPanel>
             <TabPanel value={value} index={2}>
-              Item Three
+              <Stack>
+                <Stack direction="row">
+                  <TextField
+                    placeholder="Поиск по участникам"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <SearchIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                    variant="outlined"
+                  />
+                  <Button> Добавить участников</Button>
+                </Stack>
+              </Stack>
             </TabPanel>
             <Stack
               direction="row"
