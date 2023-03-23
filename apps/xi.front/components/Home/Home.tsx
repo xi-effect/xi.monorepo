ff5997af6b5130bdfa9e601e5c2f0f7cab8fd0bd
mariik <m.portnykh@fxdev.sk>
2023-03-23 13:51:26 +0100

    feat: create calendar component

--- apps/xi.front/components/Home/Home.tsx
+++ apps/xi.front/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import Communities from './Communities';
 import Header from './Header';
+import { Calendar } from './Main/Calendar';
 
 const Home = () => (
   <>
     <Header />
     <Communities />
+    <Calendar />
   </>
 );
 
---
+++ apps/xi.front/components/Home/Main/Calendar/Calendar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import { Stack } from '@mui/material';
+
+const Calendar = observer(() => <Stack>Calendar</Stack>);
+
+export default Calendar;
