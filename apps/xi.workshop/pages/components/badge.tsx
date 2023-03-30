06b54e40326b3cb80f198de464644f58b4e93e46
unknownproperty <igor.buksheff@yandex.ru>
2023-03-30 19:46:22 +0300

    fix: build

--- apps/xi.workshop/pages/components/badge.tsx
+++ apps/xi.workshop/pages/components/badge.tsx
@@ -4,26 +4,27 @@ import { Camera } from 'pkg.icons.camera';
 
 const TestComponents = () => (
   <LayoutPages>
-    <Badge
-      icon={Camera}
-      bgColor="primary.dark"
-      iconColor="grayscale.0"
-      text="Title"
-      fontColor="grayscale.0"
-    />
+    <Badge icon={Camera} bgColor="primary.dark" iconColor="grayscale.0" fontColor="grayscale.0">
+      Title
+    </Badge>
 
-    <Badge icon={Camera} bgColor="primary.dark" iconColor="grayscale.0" />
+    <Badge icon={Camera} bgColor="primary.dark" iconColor="grayscale.0">
+      Title
+    </Badge>
 
-    <Badge size="small" icon={Camera} bgColor="grayscale.10" iconColor="error.dark" />
+    <Badge size="small" icon={Camera} bgColor="grayscale.10" iconColor="error.dark">
+      Title
+    </Badge>
 
     <Badge
       size="small"
       icon={Camera}
       bgColor="grayscale.10"
       iconColor="error.dark"
-      text="Title"
       fontColor="error.dark"
-    />
+    >
+      Title
+    </Badge>
   </LayoutPages>
 );
 
