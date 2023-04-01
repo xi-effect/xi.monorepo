67673f0f2abf9686dc7ce7ce3442370462a4a224
unknownproperty <igor.buksheff@yandex.ru>
2023-04-01 23:55:47 +0300

    feat: update Avatars

--- packages/pkg.data.avatar/Avatar.tsx
+++ packages/pkg.data.avatar/Avatar.tsx
@@ -1,29 +1,45 @@
-import { Stack } from '@mui/material';
-import Image from 'next/image';
+import { Stack, Typography } from '@mui/material';
 
 export type AvatarPropsT = {
   size?: number;
-  label?: string;
   name?: string;
+  label?: string;
+  sx?: object;
 };
 
-export const Avatar = ({ size = 48, label = 'L', name = 'rauchg' }: AvatarPropsT) => (
+function stringToHslColor(str: string, s: number, l: number) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    // eslint-disable-next-line no-bitwise
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const h = hash % 360;
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+export const Avatar = ({ size = 48, name = 'xi.effect', label = 'М', sx }: AvatarPropsT) => (
   <Stack
     direction="column"
     justifyContent="center"
     alignItems="center"
     sx={{
-      width: size,
-      height: size,
-      borderRadius: size * 0.5,
+      width: `${size}px`,
+      height: `${size}px`,
+      borderRadius: `${size / 0.5}px`,
+      bgcolor: stringToHslColor(name, 30, 80),
+      ...sx,
     }}
   >
-    <Image
-      alt="User avatar"
-      height={size}
-      width={size}
-      style={{ borderRadius: '50%' }}
-      src={`https://avatar.vercel.sh/${name}.png?size=${size}&text=${label}`}
-    />
+    <Typography
+      sx={{
+        fontWeight: 600,
+        fontSize: `${size / 2.5}px`,
+        lineHeight: `${size / 2.5}px`,
+        color: 'grayscale.0',
+      }}
+    >
+      {label}
+    </Typography>
   </Stack>
 );
