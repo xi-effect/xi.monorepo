43e549934dd03cc7f426cad3b887880e61792a43
alexAskerko <askerko50@gmail.com>
2023-04-04 08:21:48 +0300

    fix(9328294):fix participants

--- apps/xi.front/kit/CommunityProfile/ContentItems/Participants.tsx
+++ apps/xi.front/kit/CommunityProfile/ContentItems/Participants.tsx
@@ -1,39 +1,36 @@
 import { Stack, Typography } from '@mui/material';
 
 import { observer } from 'mobx-react';
-import { useStore } from 'store/connect';
+// import { useStore } from 'store/connect';
 
-const Participants = observer(() => {
-  const rootStore = useStore();
+const Participants = observer(() => (
+  // const rootStore = useStore();
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { userSt } = rootStore;
+  // const { userSt } = rootStore;
 
-  return (
-    <Stack
-      direction="row"
-      justifyContent="flex-start"
-      alignItems="center"
+  <Stack
+    direction="row"
+    justifyContent="flex-start"
+    alignItems="center"
+    sx={{
+      bgcolor: 'grayscale.0',
+      width: '100%',
+      height: '120px',
+      borderRadius: '8px',
+      padding: '24px 36px',
+    }}
+  >
+    <Typography
       sx={{
-        bgcolor: 'grayscale.0',
-        width: '100%',
-        height: '120px',
-        borderRadius: '8px',
-        padding: '24px 36px',
+        fontWeight: 600,
+        fontSize: '24px',
+        lineHeight: '32px',
+        ml: 2,
       }}
     >
-      <Typography
-        sx={{
-          fontWeight: 600,
-          fontSize: '24px',
-          lineHeight: '32px',
-          ml: 2,
-        }}
-      >
-        Участники
-      </Typography>
-    </Stack>
-  );
-});
+      Участники
+    </Typography>
+  </Stack>
+));
 
 export default Participants;
