bac6d00dcd3179ae91c0219a0014ebba59b4359e
mariik <m.portnykh@fxdev.sk>
2023-04-06 10:22:35 +0200

    fix: chat frame styles

--- apps/xi.front/pages/community/[comid]/chat/[chat].tsx
+++ apps/xi.front/pages/community/[comid]/chat/[chat].tsx
@@ -3,41 +3,38 @@ import { observer } from 'mobx-react';
 import { LayoutPages } from 'pkg.layout.pages';
 import { Chat as ChatPkg } from 'pkg.pages.chat';
 import { defaultChatData } from 'components/Community/Chat/data';
-import { Stack, useTheme } from '@mui/material';
+import { Stack } from '@mui/material';
 
-const Chat = observer(() => {
-  const { breakpoints } = useTheme();
-
-  return (
-    <LayoutPages noIndex>
-      <Navigation>
-        <Stack
-          direction="column"
-          justifyContent="flex-end"
-          alignItems="flex-start"
-          sx={{
-            height: '100vh',
-            width: '100%',
-            [breakpoints.up('xs')]: {
-              p: 3,
-              pr: 0,
-            },
-            [breakpoints.up('md')]: {
-              p: 3,
-              pr: 2.5,
-            },
-            [breakpoints.up('lg')]: {
-              p: 4,
-              pr: 3.5,
-            },
-            overflow: 'auto',
-          }}
-        >
-          <ChatPkg {...defaultChatData} />
-        </Stack>
-      </Navigation>
-    </LayoutPages>
-  );
-});
+const Chat = observer(() => (
+  <LayoutPages noIndex>
+    <Navigation>
+      <Stack
+        direction="column"
+        justifyContent="flex-end"
+        alignItems="flex-start"
+        sx={{
+          height: '100vh',
+          width: '100%',
+          p: '8px 16px',
+          // [breakpoints.up('xs')]: {
+          //   p: 3,
+          //   pr: 0,
+          // },
+          // [breakpoints.up('md')]: {
+          //   p: 3,
+          //   pr: 2.5,
+          // },
+          // [breakpoints.up('lg')]: {
+          //   p: 4,
+          //   pr: 3.5,
+          // },
+          overflow: 'auto',
+        }}
+      >
+        <ChatPkg {...defaultChatData} />
+      </Stack>
+    </Navigation>
+  </LayoutPages>
+));
 
 export default Chat;
--- packages/pkg.pages.chat/Chat.tsx
+++ packages/pkg.pages.chat/Chat.tsx
@@ -3,7 +3,12 @@ import { DateBlock } from './DateBlock';
 import { ChatProps } from './types';
 
 export const Chat = ({ name, body }: ChatProps) => (
-  <Stack direction="column" spacing={3} justifyContent="flex-end" sx={{ bgcolor: 'grayscale.0' }}>
+  <Stack
+    direction="column"
+    spacing={3}
+    justifyContent="flex-end"
+    sx={{ bgcolor: 'grayscale.0', height: '100%', overflow: 'auto', borderRadius: '8px' }}
+  >
     <Stack sx={{ position: 'fixed', top: '16px' }}>{name}</Stack>
     {body.map((data) => (
       <DateBlock {...data} />
