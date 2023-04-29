eb96625ba1428b84c966f477c8af365d6256e609
unknownproperty <igor.buksheff@yandex.ru>
2023-04-29 20:54:45 +0300

    feat: some changes for EmojiPicker

--- apps/xi.workshop/pages/components/emoji.tsx
+++ apps/xi.workshop/pages/components/emoji.tsx
@@ -1,11 +1,21 @@
-import { Grid as GridLayout } from '@mui/material';
+import { Grid as GridLayout, Box } from '@mui/material';
 import { LayoutPages } from 'kit/LayoutPages';
-import { EmojiList } from 'pkg.emoji.picker';
+import { EmojiList, EmojiPicker } from 'pkg.emoji.picker';
 
 const Emoji = () => (
   <LayoutPages>
-    <GridLayout sx={{ p: 4, maxWidth: '624px' }} container spacing={4}>
+    <GridLayout sx={{ p: 4, maxWidth: '1624px' }} container flexDirection="column" spacing={4}>
       <EmojiList />
+      <Box
+        sx={{
+          p: 6,
+          marginTop: '300px',
+          height: '600px',
+          width: '600px',
+        }}
+      >
+        <EmojiPicker />
+      </Box>
     </GridLayout>
   </LayoutPages>
 );
